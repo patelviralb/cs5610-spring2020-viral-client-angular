f7525e1da8edc6dcfd36001575ceaa0b9af0a84c
patelviralb <wsl-ubuntu1804@LAPTOP-8C9N2MUK.localdomain>
2020-03-17 17:00:48 -0400

    Added course-table

--- src/app/components/course-table/course-table.component.ts
+++ src/app/components/course-table/course-table.component.ts
@@ -9,7 +9,15 @@ export class CourseTableComponent implements OnInit {
 
   constructor() { }
 
+  courses = [
+    {"id": 1, "title": "Course 01"},
+    {"id": 2, "title": "Course 02"},
+    {"id": 3, "title": "Course 03"},
+    {"id": 4, "title": "Course 04"}
+  ]
+
   ngOnInit(): void {
+    
   }
 
 }
