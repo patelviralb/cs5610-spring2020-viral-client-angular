4b646f862fbb3fc12af7d86f06b7b3dbbb58f86d
patelviralb <wsl-ubuntu1804@LAPTOP-8C9N2MUK.localdomain>
2020-03-17 17:01:34 -0400

    Added course-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseTableComponent } from './components/course-table/course-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseTableComponent
   ],
   imports: [
     BrowserModule,
