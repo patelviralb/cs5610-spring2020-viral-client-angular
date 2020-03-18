33272e5a22ec48fddd9b574f9f3409b7e7800e03
patelviralb <patel.viral@husky.neu.edu>
2020-03-18 19:24:39 -0400

    Added Home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent} from '../app/components/home/home.component';
 import { CourseTableComponent } from './components/course-table/course-table.component';
 import { CourseViewerComponent } from './components/course-viewer/course-viewer.component';
 
 
 const routes: Routes = [
+  {path:'', component: HomeComponent},
   {path:'table/courses', component: CourseTableComponent},
   {path:':layout/courses/:courseId/modules', component: CourseViewerComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { CourseTableComponent } from './components/course-table/course-table.component';
 import {CourseServiceClient} from '../app/services/CourseServiceClient';
 import { CourseViewerComponent } from './components/course-viewer/course-viewer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseTableComponent,
-    CourseViewerComponent
+    CourseViewerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
