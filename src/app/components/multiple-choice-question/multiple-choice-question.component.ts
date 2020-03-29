25929d399f79d494405df057a44003db99e7b686
patelviralb <patel.viral@husky.neu.edu>
2020-03-28 22:01:07 -0400

    Completed Multiple Choice Question type

--- src/app/components/multiple-choice-question/multiple-choice-question.component.ts
+++ src/app/components/multiple-choice-question/multiple-choice-question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-multiple-choice-question',
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class MultipleChoiceQuestionComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  question;
+  selectedAnswer = '';
+  gradeAnswer = false;
+
+  grade = () => {
+    this.gradeAnswer = true;
+  };
 
   ngOnInit(): void {
   }
