d3328d92b3203539c9b9b99dff4eaf4d79d54ac8
patelviralb <patel.viral@husky.neu.edu>
2020-04-08 19:17:50 -0400

    Updated Heroku Link for Quizzes

--- src/app/common/Constants.ts
+++ src/app/common/Constants.ts
@@ -4,6 +4,6 @@ import {Injectable} from '@angular/core';
 export class Constants {
   COURSE_API_URL = 'https://wbdv-generic-server.herokuapp.com/api/001374158/courses';
   MODULE_API_URL = 'https://wbdv-generic-server.herokuapp.com/api/001374158/modules';
-  /*QUIZ_API_URL = 'https://vp-cs5610-sp2020a9-server-node.herokuapp.com/api/quizzes';*/
-  QUIZ_API_URL = 'http://localhost:5000/api/quizzes';
+  QUIZ_API_URL = 'https://vp-cs5610-sp2020a9-server-node.herokuapp.com/api/quizzes';
+  /*QUIZ_API_URL = 'http://localhost:5000/api/quizzes';*/
 }
